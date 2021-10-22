ec3fa06d1b7059471c92524e81c0d33d81586080
n57r <grntboi90@gmail.com>
2021-10-22 09:43:28 -0700

    Initial comit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/results',function (req,res) {
+  console.log(req.body.description);
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+  console.log(req.body.type);
+  console.log(req.body.dateOfBirth);
+  console.log(req.body.Yes);
+  console.log(req.body.No);
+  res.render('index')
+});
+module.exports = router;
